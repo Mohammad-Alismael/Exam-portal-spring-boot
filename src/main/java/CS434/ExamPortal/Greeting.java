9660bd3d58f2bac4e33aa5464c74fe648cad4281
mhd <mhdhd@gmail.com>
2021-10-24 16:47:49 +0400

    adding uuid class generator

--- src/main/java/CS434/ExamPortal/Greeting.java
+++ src/main/java/CS434/ExamPortal/Greeting.java
@@ -1,15 +1,15 @@
 package CS434.ExamPortal;
 
 public class Greeting {
-    private final long id;
+    private final String id;
     private final String content;
 
-    public Greeting(long id, String content) {
+    public Greeting(String id, String content) {
         this.id = id;
         this.content = content;
     }
 
-    public long getId() {
+    public String getId() {
         return id;
     }
 
--- src/main/java/CS434/ExamPortal/GreetingController.java
+++ src/main/java/CS434/ExamPortal/GreetingController.java
@@ -1,5 +1,6 @@
 package CS434.ExamPortal;
 
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,14 @@ public class GreetingController {
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        return new Greeting(RandomUuidStringCreator.getRandomUuid(), String.format(template, name));
+    }
+    @GetMapping("/listGreeting")
+    public ArrayList<Greeting> greeting() {
+        ArrayList<Greeting> g = new ArrayList<>();
+        g.add(new Greeting(RandomUuidStringCreator.getRandomUuid(), "hello"));
+        g.add(new Greeting(RandomUuidStringCreator.getRandomUuid(), "hello2"));
+        g.add(new Greeting(RandomUuidStringCreator.getRandomUuid(), "hello3"));
+        return g;
     }
 }
