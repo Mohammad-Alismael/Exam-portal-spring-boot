804c3de9cabbbf70ba4fd52955cafd4784fc63e0
mhd <mhdhd@gmail.com>
2021-11-20 19:02:12 +0400

    authentication system

--- src/main/java/CS434/ExamPortal/Controller/UserController.java
+++ src/main/java/CS434/ExamPortal/Controller/UserController.java
@@ -3,12 +3,34 @@ package CS434.ExamPortal.Controller;
 import CS434.ExamPortal.DAO.Users;
 import CS434.ExamPortal.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+
+    @PostMapping(path="/authenticate") // Map ONLY POST Requests
+    public @ResponseBody
+    List<Users> authenticateUser (@RequestBody Users userInfo) {
+        List<Users> a = userRepository.
+                findUsersByUsernameAndAndPassword(
+                        userInfo.getUsername(),
+                        userInfo.getPassword());
+        if (a.size() == 0){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "incorrect username or password");
+        }
+
+        return userRepository.
+                findUsersByUsernameAndAndPassword(
+                        userInfo.getUsername(),
+                        userInfo.getPassword());
+
+    }
 
     @GetMapping(path="/list")
     public @ResponseBody Iterable<Users>test() {
@@ -17,9 +39,16 @@ public class UserController {
 
     @PostMapping(path="/add") // Map ONLY POST Requests
     public @ResponseBody
-    String addNewUser (@RequestBody Users userInfo) {
-        userRepository.save(userInfo);
-        return "Saved";
+    Users addNewUser (@RequestBody Users userInfo) {
+        Users users = userRepository.findByUsername(userInfo.getUsername());
+
+        if (users == null){
+            userRepository.save(userInfo);
+            return users;
+        }else {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "username already taken");
+        }
+
     }
 
 }
--- src/main/java/CS434/ExamPortal/Repositories/UserRepository.java
+++ src/main/java/CS434/ExamPortal/Repositories/UserRepository.java
@@ -3,5 +3,9 @@ package CS434.ExamPortal.Repositories;
 import CS434.ExamPortal.DAO.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<Users,Integer> {
+    List<Users> findUsersByUsernameAndAndPassword(String username, String password);
+    Users findByUsername(String username);
 }
