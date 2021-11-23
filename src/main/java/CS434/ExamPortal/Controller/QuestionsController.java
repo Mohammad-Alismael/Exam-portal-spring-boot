9611f976f6fc193e0514e819cb3dc0253e7f2727
mhd <mhdhd@gmail.com>
2021-11-23 17:08:38 +0400

    adding few endpoints for questions2.0

--- src/main/java/CS434/ExamPortal/Controller/QuestionsController.java
+++ src/main/java/CS434/ExamPortal/Controller/QuestionsController.java
@@ -4,10 +4,13 @@ import CS434.ExamPortal.DAO.Questions;
 import CS434.ExamPortal.Repositories.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 public class QuestionsController {
     @Autowired
@@ -21,7 +24,7 @@ public class QuestionsController {
        }
        questionRepository.save(question);
 
-       return  new ResponseStatusException(HttpStatus.ACCEPTED);
+       return  new ResponseStatusException(HttpStatus.CREATED);
 
     }
 
@@ -35,11 +38,12 @@ public class QuestionsController {
         quest.setQuestionText(question.getQuestionText());
         quest.setPoints(question.getPoints());
         // the rest
+        questionRepository.save(quest);
         return  new ResponseStatusException(HttpStatus.ACCEPTED);
 
     }
 
-    @PostMapping(path="/delete-question") // Map ONLY POST Requests
+    @PostMapping(path="/delete-question")
     public @ResponseBody
     ResponseStatusException deleteQuestion (@RequestBody Questions question) {
         if (question.getCreatorExamId() != 1){
@@ -50,5 +54,11 @@ public class QuestionsController {
         questionRepository.save(quest);
         return  new ResponseStatusException(HttpStatus.ACCEPTED);
 
+    }
+
+    @GetMapping (path="/list-questions")
+    public @ResponseBody
+    List<Questions> listQuestions (@RequestBody String examId) {
+        return  questionRepository.findQuestionsByCreatorExamIdAndIsActive(examId,true);
     }
 }
