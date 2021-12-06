ab5b89c453d09d6593d887c28260cfd038f64afc
mhd <mhdhd@gmail.com>
2021-12-06 22:01:11 +0400

    fixing ClassroomController endpoint

--- src/main/java/CS434/ExamPortal/Controller/ClassroomController.java
+++ src/main/java/CS434/ExamPortal/Controller/ClassroomController.java
@@ -1,7 +1,11 @@
 package CS434.ExamPortal.Controller;
 
 import CS434.ExamPortal.DAO.Classroom;
+import CS434.ExamPortal.DAO.Users;
 import CS434.ExamPortal.Repositories.ClassroomRepository;
+import CS434.ExamPortal.Repositories.UserRepository;
+import CS434.ExamPortal.RepositoriesImplement.UserRepositoryImpl;
+import CS434.ExamPortal.behavioralPattern.nullObject.NullUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,22 +22,28 @@ public class ClassroomController {
 
     @Autowired
     private ClassroomRepository classroomRepository;
-
+    @Autowired
+    private UserRepositoryImpl userRepositoryImpl;
+    @Autowired
+    private UserRepository userRepository;
     @PostMapping("/set-classroom-to-students")
     public ResponseStatusException setClassroomStudents(@RequestBody Classroom classroom) {
+        NullUser user = userRepositoryImpl.findByUserId(classroom.getStudentId());
+        if (!user.isAvailable()) throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"student id doesn't exists");
         classroomRepository.save(classroom);
         return new ResponseStatusException(HttpStatus.ACCEPTED);
     }
 
-    @GetMapping("/hgjjtuj64")
-    public Classroom hello123(@RequestBody Classroom classroom) {
-//        classroomRepository.save(classroom);
-        return new Classroom();
-    }
 
     @GetMapping("/get-class-students")
-    public List<Classroom> getClassStudents(@RequestBody Classroom classroom) {
-        return classroomRepository.findClassroomByInstructorId(classroom.getInstructorId());
+    public ArrayList<Users> getClassStudents(@RequestBody Classroom classroom) {
+        List<Classroom> classrooms = classroomRepository.findClassroomByInstructorId(classroom.getInstructorId());
+        ArrayList<Users> users = new ArrayList<>();
+        for (Classroom classroom1 : classrooms){
+            Users u = userRepository.findByUserIdv2(classroom1.getStudentId());
+            users.add(u);
+        }
+        return users;
     }
 
     @GetMapping("/get-class-instructor")
@@ -41,10 +52,16 @@ public class ClassroomController {
     }
 
     @PostMapping("/delete-class-instructor")
-    public void deleteClassInstructor(@RequestBody Classroom classroom) {
+    public RuntimeException deleteClassInstructor(@RequestBody Classroom classroom) {
         classroomRepository.removeByInstructorIdAndStudentId(
                 classroom.getInstructorId(),
                 classroom.getStudentId()
         );
+        return new ResponseStatusException(HttpStatus.GONE,"has been deleted successfully!");
+    }
+
+    @GetMapping("/get-all-classrooms")
+    public List<Classroom> getAlClassInstructor() {
+        return classroomRepository.findAll();
     }
 }
--- src/main/java/CS434/ExamPortal/Repositories/ClassroomRepository.java
+++ src/main/java/CS434/ExamPortal/Repositories/ClassroomRepository.java
@@ -1,14 +1,21 @@
 package CS434.ExamPortal.Repositories;
 
 import CS434.ExamPortal.DAO.Classroom;
+import CS434.ExamPortal.DAO.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface ClassroomRepository extends JpaRepository<Classroom,Integer> {
 
     List<Classroom> findClassroomByInstructorId(Integer instructorID);
-
+    @Transactional
     void  removeByInstructorIdAndStudentId(Integer instructorID, Integer studentId);
 
+    @Query(value = "select username,email_id from Users join Classroom C on Users.user_id = C.student_id where instructor_id=:instructorID;",
+            nativeQuery = true)
+    List<Users> listStudentsUsernamesByInstructorId(@Param("instructorID") Integer instructorID);
 }
